a07a7a607784b578c8275db5400ac26bd1fe335e
Dennis Somerville <densom@users.noreply.github.com>
2024-06-04 23:17:20 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="densom-saysomething",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=[],  # Add any dependencies here
     author="Dennis Somerville",
