0e6d16cbbae09ace31db7804cbe987d54348e28e
Dennis Somerville <densom@users.noreply.github.com>
2024-06-04 22:58:50 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
     author="Dennis Somerville",
     # author_email="your.email@example.com",
     description="A simple text to speech implementation.",
+    long_description_content_type="text/markdown",
     url="https://github.com/densom/saysomething",
 )
